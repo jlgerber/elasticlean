397618e6871cbe5db33adec4e01e89846659efa2
jonathan gerber <jlgerber@gmail.com>
2018-10-16 23:59:28 -0700

    add EcError

---
+++ src/errors.rs
@@ -0,0 +1,10 @@
+
+#[derive(Fail, Debug,PartialEq,Eq, PartialOrd, Ord)]
+pub enum EcError {
+    #[fail(display = "failed to parse: {}", _0)]
+    ParseError(String),
+    #[fail(display = "reqwest get error: {}", _0)]
+    ReqwestGetError(String),
+    #[fail(display = "reqwest json error. unable to deserialize to json: {}", _0)]
+    ReqwestJsonError(String),
+}
